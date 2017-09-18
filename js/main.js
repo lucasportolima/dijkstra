32b767219520bc0beb8a7b2c3558927dfdaf51c8
lucasportolima@live.com <lucasportolima@live.com>
2017-09-18 01:08:10 -0300

    Foi criado as função de add partida e add chegada

--- js/main.js
+++ js/main.js
@@ -116,8 +116,18 @@ document.getElementById('cliente19').style.top = x4y3Y;
 
 
 // função que add a partida
-function add_partida(x,y) {
+function add_partida() {
 	//falta verificar se os vertices são válidos
+	arrayX = [0,15,30,45,50]
+	arrayY = [0,20,50,80]
+	x = parseInt(document.getElementById('inputInicioX').value)
+	y = parseInt(document.getElementById('inputInicioY').value)
+
+	if (arrayX.indexOf(x) < 0 && arrayY.indexOf(y) < 0) {
+		alert("Valor de partida invalido, digite outro valor!")
+		return 0;
+	}
+
 	if (x == 0) {
 		document.getElementById('partida').classList.add("cliente");
 		document.getElementById('partida').style.left = (document.getElementById('C-1').offsetLeft - 10) + 'px';
@@ -171,7 +181,72 @@ function add_partida(x,y) {
 //função add chegada, função igual da add_partida, com a diferença que
 //ela vai verificar se a chegada está em cima da partida.
 function add_chegada() {
+	arrayX = [0,15,30,45,50]
+	arrayY = [0,20,50,80]
+	xPartida = parseInt(document.getElementById('inputInicioX').value)
+	yPartida = parseInt(document.getElementById('inputInicioY').value)
 
+	x = parseInt(document.getElementById('inputFinalX').value)
+	y = parseInt(document.getElementById('inputFinalY').value)
+
+	if (xPartida == x && yPartida == y) {
+		alert("Valores de chegadas são iguais aos valores de partida!")
+		return 0;
+	}
+
+	if (arrayX.indexOf(x) < 0 && arrayY.indexOf(y) < 0) {
+		alert("Valor de chegada invalido, digite outro valor!")
+		return 0;
+	}
+
+	if (x == 0) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-1').offsetLeft - 10) + 'px';
+	}else if (x > 0 && x < 15) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-1').offsetLeft + (parseInt(document.getElementById('C-1').offsetWidth/14) * x)) + 'px';
+	}else if (x == 15) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-2').offsetLeft - 10) + 'px';
+	}else if (x > 15 && x < 30) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-2').offsetLeft + (parseInt(document.getElementById('C-2').offsetWidth/14) * (x - 15))) + 'px';
+	}else if (x == 30) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-3').offsetLeft - 10) + 'px';
+	}else if (x > 30 && x < 45) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-3').offsetLeft + (parseInt(document.getElementById('C-3').offsetWidth/14) * (x - 30))) + 'px';
+	}else if (x == 45) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-4').offsetLeft - 10) + 'px';
+	}else if (x > 45 && x < 50) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-4').offsetLeft + (parseInt(document.getElementById('C-4').offsetWidth/4) * (x - 45))) + 'px';
+	}else if (x == 50) {
+		document.getElementById('partida').classList.add("cliente");
+		document.getElementById('partida').style.left = (document.getElementById('C-4').offsetLeft + document.getElementById('C-4').offsetWidth ) + 'px';
+	}else {
+		alert("valor invalido");
+	}
+
+	if (y == 0) {
+		document.getElementById('partida').style.top = (document.getElementById('C-1').offsetTop + document.getElementById('C-1').offsetHeight + 10) + 'px';
+	}else if (y > 0 && y < 20) {
+		document.getElementById('partida').style.top = (document.getElementById('C-1').offsetTop) + (parseInt(document.getElementById('C-1').offsetHeight/19) * y) + 'px';
+	}else if (y == 20) {
+		document.getElementById('partida').style.top = (document.getElementById('C-1').offsetTop) + 'px';
+	}else if (y > 20 && y < 50) {
+		document.getElementById('partida').style.top = (document.getElementById('B-1').offsetTop) + (parseInt(document.getElementById('B-1').offsetHeight/29) * (50 - y)) + 'px';
+	}else if (y == 50) {
+		document.getElementById('partida').style.top = document.getElementById('B-1').offsetTop + 'px';
+	}else if (y > 50 && y < 80) {
+		document.getElementById('partida').style.top = (document.getElementById('A-1').offsetTop) + (parseInt(document.getElementById('A-1').offsetHeight/29) * (80 - y)) + 'px';
+	}else if (y == 80) {
+		document.getElementById('partida').style.top = document.getElementById('A-1').offsetTop + 'px';
+	}else {
+		alert("valor invalido");
+	}
 }
 
 //função bloqueio: vai ser add um pontinho no bloqueio, vai ser virificado se
